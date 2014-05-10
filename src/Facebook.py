f56d10ef3bf73b6b4714f96beb8503e6db374a31
P-Raj <pranav09032@hotmail.com>
2014-05-10 12:23:57 +0530

    Facebook.py : more 'pythonic'

--- src/Facebook.py
+++ src/Facebook.py
@@ -14,9 +14,19 @@ class Facebook ():
         self.name = None
         self.baseUrl = "https://graph.facebook.com/"
 
+    def urlRead(self, url):
+
+        try:
+            response = self.urlRead(url)
+            return response
+
+        except ConnectionError:
+            raise Exception("Connection Exception")
+
     def readTag(self, user, tag=""):
+
         return json.load(
-            urllib.urlopen(
+            self.urlRead(
                 self.baseUrl
                 + "/" + tag + "?"
                 + urllib.urlencode(
@@ -24,41 +34,36 @@ class Facebook ():
                          self.accessToken))))
 
     def userInformation(self, fbUser="me"):
-        """
-        returns User Information ; fbUser can be the facebook id of any user ;
-        for the current user it is 'me'
-        """
 
         profile = self.readTag(fbUser, "")
-        self.id = str(profile["id"])
-        self.name = str(profile["name"])
-        user = dict(key_name=str(profile["id"]),
-                    id=str(profile["id"]),
-                    name=profile["name"],
-                    accessToken=self.accessToken,
-                    profile_url=profile["link"])
-        return user
+        self.id = profile.get("id", None)
+        self.name = profile.get("name", None)
 
-    def getFriends(self, maxPage=4, fbUser="me"):
-        """
-        Gets the list of friends , maxPage : Pages need to be accessed ;
-        4 can cober most of the friendlist ; will ensure that data is
-        returned within 60 secs
-        """
-        friends = self.readTag(fbUser, "friends")
-        Friends = []
-        while "next" in friends["paging"]:
-            if maxPage == 0:
+        if self.id and self.name:
+            user = dict(key_name=str(profile["id"]),
+                        id=str(profile["id"]),
+                        name=profile["name"],
+                        accessToken=self.accessToken,
+                        profile_url=profile["link"])
+            return user
+
+        raise Exception("Profile id or name is None")
+
+    def getFriends(self, maxParsedPages=4, fbUser="me"):
+
+        friends_info = self.readTag(fbUser, "friends")
+        friends = []
+        while "next" in friends_info["paging"]:
+            if not maxParsedPages:
                 break
-            maxPage = maxPage - 1
-            Friends = Friends + friends["data"]
-            friends = json.load(urllib.urlopen(friends["paging"]["next"]))
-        return Friends
+            maxParsedPages -= 1
+            friends += friends_info["data"]
+            friends_info = json.load(
+                self.urlRead(
+                    friends_info["paging"]["next"]))
+        return friends
 
     def getUserWall(self, max_pages=2, fbUser="me"):
-        """
-        gets User Wall
-        """
 
         user_feeds = self.readTag(fbUser, "feed")
         Data = []
@@ -66,7 +71,7 @@ class Facebook ():
             try:
                 Data = Data + user_feeds["data"]
                 user_feeds = json.load(
-                    urllib.urlopen(user_feeds["paging"]["next"]))
+                    self.urlRead(user_feeds["paging"]["next"]))
                 max_pages = max_pages - 1
                 if max_pages < 1:
                     break
@@ -74,124 +79,56 @@ class Facebook ():
                 break
         return Data
 
+    def __getUserComment(self, fbUser, tag):
+
+        UserWall = self.getUserWall(fbUser=fbUser)
+        return [feed[tag] for feed in UserWall if tag in feed]
+
     def getUserCommentsStory(self, fbUser="me"):
         """
         gets User Comments
         """
-
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = [feed["story"] for feed in UserWall if "story" in feed]
-        return returnData
+        return self.__getUserComment(fbUser, "story")
 
     def getUserCommentsPicture(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "picture" in feed:
-                returnData.append(feed["picture"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "picture")
 
     def getUserCommentsFrom(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "from" in feed:
-                returnData.append(feed["from"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "from")
 
     def getUserCommentsName(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "name" in feed:
-                returnData.append(feed["name"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "name")
 
     def getUserCommentsCaption(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "caption" in feed:
-                returnData.append(feed["caption"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "caption")
 
     def getUserCommentsDescription(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "description" in feed:
-                returnData.append(feed["description"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "description")
 
     def getUserCommentsComments(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "comments" in feed:
-                returnData.append(feed["comments"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "comments")
 
     def getUserCommentsUpdatedTime(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "updated_time" in feed:
-                returnData.append(feed["updated_time"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "updated_time")
 
     def getUserCommentsType(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "type" in feed:
-                returnData.append(feed["type"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "type")
 
     def getUserCommentsId(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "id" in feed:
-                returnData.append(feed["id"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "id")
 
     def getUserCommentsLikes(self, fbUser="me"):
 
-        UserWall = self.getUserWall(fbUser=fbUser)
-        returnData = []
-
-        for feed in UserWall:
-            if "likes" in feed:
-                returnData.append(feed["likes"])
-
-        return returnData
+        return self.__getUserComment(fbUser, "likes")
 
     def getUserLikes(self, fbUser="me"):
 
@@ -200,10 +137,10 @@ class Facebook ():
 
         while True:
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_likes["data"]]
+                returnData += [x["name"] for x in user_likes["data"]]
                 user_likes = json.load(
-                    urllib.urlopen(user_likes["paging"]["next"]))
+                    self.urlRead(
+                        user_likes["paging"]["next"]))
             except:
                 break
 
@@ -217,10 +154,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_movies["data"]]
+                returnData += [x["name"] for x in user_movies["data"]]
                 user_movies = json.load(
-                    urllib.urlopen(user_movies["paging"]["next"]))
+                    self.urlRead(
+                        user_movies["paging"]["next"]))
             except:
                 break
 
@@ -234,10 +171,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_music["data"]]
+                returnData += [x["name"] for x in user_music["data"]]
                 user_music = json.load(
-                    urllib.urlopen(user_music["paging"]["next"]))
+                    self.urlRead(
+                        user_music["paging"]["next"]))
             except:
                 break
 
@@ -251,10 +188,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_book["data"]]
+                returnData += [x["name"] for x in user_book["data"]]
                 user_book = json.load(
-                    urllib.urlopen(user_book["paging"]["next"]))
+                    self.urlRead(
+                        user_book["paging"]["next"]))
             except:
                 break
 
@@ -268,10 +205,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_notes["data"]]
+                returnData += [x["name"] for x in user_notes["data"]]
                 user_notes = json.load(
-                    urllib.urlopen(user_notes["paging"]["next"]))
+                    self.urlRead(
+                        user_notes["paging"]["next"]))
             except:
                 break
 
@@ -285,10 +222,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_photos["data"]]
+                returnData += [x["name"] for x in user_photos["data"]]
                 user_photos = json.load(
-                    urllib.urlopen(user_photos["paging"]["next"]))
+                    self.urlRead(
+                        user_photos["paging"]["next"]))
             except:
                 break
 
@@ -302,9 +239,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + [x for x in user_notes["data"]]
+                returnData += [x for x in user_notes["data"]]
                 user_notes = json.load(
-                    urllib.urlopen(user_notes["paging"]["next"]))
+                    self.urlRead(
+                        user_notes["paging"]["next"]))
             except:
                 break
 
@@ -318,10 +256,10 @@ class Facebook ():
         while True:
 
             try:
-                returnData = returnData + \
-                    [x["name"] for x in user_notes["data"]]
+                returnData += [x["name"] for x in user_notes["data"]]
                 user_notes = json.load(
-                    urllib.urlopen(user_notes["paging"]["next"]))
+                    self.urlRead(
+                        user_notes["paging"]["next"]))
             except:
                 break
 
@@ -342,7 +280,8 @@ class Facebook ():
                     else:
                         returnData[place] = 0
                 user_places = json.load(
-                    urllib.urlopen(user_places["paging"]["next"]))
+                    self.urlRead(
+                        user_places["paging"]["next"]))
             except:
                 break
 
